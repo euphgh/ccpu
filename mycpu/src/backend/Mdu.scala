9224fccfb4938222f90d45bffe0d593344f9e629
luot <1035768203@qq.com>
2023-06-08 18:30:15 +0800

    add mul/div

--- mycpu/src/backend/Mdu.scala
+++ mycpu/src/backend/Mdu.scala
@@ -5,16 +5,37 @@ import config._
 import chisel3._
 import chisel3.util._
 
-class Mdu extends MycpuModule {
+class MulDivIO extends MycpuBundle {
+  val in = Flipped(Decoupled(new Bundle {
+    val srcs   = Vec(srcDataNum, Output(UInt(dataWidth.W)))
+    val isSign = Output(Bool())
+  }))
+  val out = DecoupledIO(Output(UInt((dataWidth * 2).W)))
+}
 
+class Multiplier extends MycpuModule {
+  val io = IO(new MulDivIO)
+  //io.out.bits
+  //io.out.valid := F(io.in.fire())
+  val busy = RegInit(false.B)
+  when(io.in.valid && !busy) { busy := true.B }
+  when(io.out.valid) { busy := false.B }
+  io.in.ready := !busy
+}
+class Divider extends MycpuModule {
+  val io = IO(new MulDivIO)
+  //automachine
+}
+
+class Mdu extends MycpuModule {
   val roStage = Module(new RoStage(fuKind = FuType.Mdu.id))
   //for now,no inside bypass
 
   //TODO:pipeline connect roStage.io<>exeStageIO
   val exeStageIO = new ExeStageIO(fuKind = FuType.Alu.id)
-  /*
-  TODO:
-    exe logic(real MDU)
-   */
+  val mul        = Module(new Multiplier)
+  val div        = Module(new Divider)
 
+  val hiReg = Mem(1, UWord)
+  val loReg = Mem(1, UWord)
 }

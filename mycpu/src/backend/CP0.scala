89274a97bac6c576f0ed4490dc879b4137b414af
luot <1035768203@qq.com>
2023-06-22 20:11:35 +0800

    [CP0]add some comment

--- mycpu/src/backend/CP0.scala
+++ mycpu/src/backend/CP0.scala
@@ -13,31 +13,29 @@ class Mtc0Bundle extends SimpleWriteBundle {
 }
 
 /**
-  * no need decoupled,always rdy
-  *
   * after inst retire from rob:
   *     if exception occurred <exception>
   *     if is a eret inst<eretFlush>
-  *     if is a mfc0 inst<c0Inst>
-  *
-  * a mtc0 inst:
+  *     if is a mtc0 <mtc0>(no need "Valid",gen wen in MDU)
+  *       use Queue in Mdu to record wdata and coAddr
+  *       ROB send an commit signal to MDU,MDU dequeue the related Queue
+  *         and send (wen,wdata,c0Addr) to CP0
+  * a mfc0 inst:
   *     can only be dispatched when rob is empty
-  *     treat as normal alu inst
-  *         but no need to do anything in RO
-  *         read c0 in Exe
+  *     deal in MDU:
+  *         do nothing in RO
+  *         read c0 in EXE
   */
 class CP0 extends MycpuModule {
   val io = IO(new Bundle {
     val in = new Bundle {
-
+      val mfc0Addr  = Input(CP0Idx)
+      val mtc0      = Flipped(new Mtc0Bundle)
+      val eretFlush = Input(Bool())
       val exception = Flipped(Valid(new Bundle {
         val basic    = new ExceptionInfoBundle
         val badVaddr = Output(UWord)
       }))
-      val eret = Input(Bool()) //to CP0
-      val mtc0 = Flipped(new Mtc0Bundle)
-
-      val mfc0Addr = Input(CP0Idx)
     }
     val out = new Bundle {
       val mfc0Data = Output(UWord) // wire logic of mfc0

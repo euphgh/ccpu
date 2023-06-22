6e6c14870d916197b19c99c6c20fa152000e9677
Guanghui Hu <120L052208@stu.hit.edu.cn>
2023-06-22 20:33:16 +0800

    [Enum]: move MemType to Enum and add BitPat for it

--- mycpu/src/config/Enum.scala
+++ mycpu/src/config/Enum.scala
@@ -1,5 +1,6 @@
 package config
 import chisel3._
+import chisel3.util._
 
 object SRCType extends ChiselEnum {
   val RSRT, RS, RT, noSRC = Value
@@ -21,4 +22,30 @@ object SpecialType extends ChiselEnum {
 }
 object BlockType extends ChiselEnum {
   val CACHEINST, MFC0, NON = Value
+}
+
+object MemType extends ChiselEnum {
+  // group word
+  val LW = Value("b0000".U)
+  val SW = Value("b0001".U)
+  // group left
+  val LWL = Value("b0010".U)
+  val SWL = Value("b0011".U)
+  // group right
+  val LWR = Value("b0100".U)
+  val SWR = Value("b0101".U)
+  // group byte0
+  val LB  = Value("b0100".U)
+  val LBU = Value("b0101".U)
+  val SB  = Value("b0110".U)
+  // group half
+  val LH  = Value("b1000".U)
+  val LHU = Value("b1001".U)
+  val SH  = Value("b1010".U)
+
+  def wordPat  = BitPat("b000?")
+  def leftPat  = BitPat("b001?")
+  def rightPat = BitPat("b010?")
+  def bytePat  = BitPat("b01??")
+  def halfPat  = BitPat("b10??")
 }

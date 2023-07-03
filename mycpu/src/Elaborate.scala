fb21afd6f60daa95b38a80cc7232174dec3b3847
Guanghui Hu <120L052208@stu.hit.edu.cn>
2023-07-03 16:36:50 +0800

    [Main]: remove FakeIF1

--- mycpu/src/Elaborate.scala
+++ mycpu/src/Elaborate.scala
@@ -14,17 +14,6 @@ object Elaborate extends App {
   val generator = Seq(chisel3.stage.ChiselGeneratorAnnotation(() => top))
   (new ChiselStage).execute(args, generator :+ CIRCTTargetAnnotation(CIRCTTarget.Verilog))
 }
-class WrapIfStage1 extends MycpuModule {
-  val update = IO(Flipped(new BpuUpdateIO))
-  val tlb    = IO(new TLBSearchIO)
-  val if1    = Module(new FakeIF1)
-  if1.io.bpuUpdateIn <> update
-  if1.io.tlb <> tlb
-  if1.io.in.npc         := if1.io.out.bits.predictResult(0).target | if1.io.out.bits.predictResult(1).target
-  if1.io.in.flush       := false.B
-  if1.io.in.isDelaySlot := if1.io.out.bits.validMask.asUInt.orR
-  if1.io.out.ready      := true.B
-}
 
 class WrapBTB extends MycpuModule {
   val update = IO(Flipped(new BpuUpdateIO))
@@ -37,7 +26,6 @@ class WrapBTB extends MycpuModule {
 }
 
 object SubMain extends App {
-
   def top       = new WrapBTB
   val generator = Seq(chisel3.stage.ChiselGeneratorAnnotation(() => top))
   (new ChiselStage).execute(args, generator :+ CIRCTTargetAnnotation(CIRCTTarget.Verilog))

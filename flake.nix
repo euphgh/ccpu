ee73b1bed206264074f77228deddc2871ab2550e
Guanghui Hu <120L052208@stu.hit.edu.cn>
2023-07-25 12:24:30 +0800

    [flake]: add vivado2019.2

--- flake.nix
+++ flake.nix
@@ -2,16 +2,19 @@
   description = "my project description";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.vivado20192.url = "github:euphgh/nur-vivado-2019.2";
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs = { self, nixpkgs, flake-utils, vivado20192, ... }:
     flake-utils.lib.eachDefaultSystem
       (system:
-        let pkgs = nixpkgs.legacyPackages.${system}; in
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in
         {
           devShells.default = with pkgs; mkShell {
             package = with pkgs; [
               gnumake
               mill
+              vivado20192.packages.${system}.default
             ];
             NIX_LD_LIBRARY_PATH = lib.makeLibraryPath [
               stdenv.cc.cc

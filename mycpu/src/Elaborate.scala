f49a7a8e973352e870912a767847e91f3488a088
Guanghui Hu <120L052208@stu.hit.edu.cn>
2023-08-11 10:29:47 +0800

    [Divider]: avg 8.8

--- mycpu/src/Elaborate.scala
+++ mycpu/src/Elaborate.scala
@@ -1,6 +1,7 @@
 import circt.stage._
+import backend.components._
 object Elaborate extends App {
-  def top       = new CCPU
+  def top       = new Divider
   val generator = Seq(chisel3.stage.ChiselGeneratorAnnotation(() => top))
   (new ChiselStage).execute(args, generator :+ CIRCTTargetAnnotation(CIRCTTarget.Verilog))
 }
